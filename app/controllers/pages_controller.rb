22cbdc24a3b6188a0985ee7bed4f347f6d678e0b
Thomas von Deyen <tvdeyen@gmail.com>
2011-06-19 23:24:59 +0200

    Added page caching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < Spree::BaseController
+  before_filter { render_404 if params[:id] =~ /(\.|\\)/ }
 
+  rescue_from ActionView::MissingTemplate, :with => :render_404
+  caches_page :show, :if => Proc.new { Spree::Config[:cache_static_content] }
+
+  respond_to :html
+  
   # GET /pages/about-us
   def show
     @page = Page.where(:published => true, :permalink => params[:id]).first
