6fca7fc046a819e4dcaf07bfe4a4fb7323b193dd
Alberto Vena <kennyadsl@gmail.com>
2012-04-03 12:39:32 +0200

    adjusts lib directory

--- lib/generators/spree_pages/install/install_generator.rb
+++ lib/generators/spree_pages/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreePages
     class InstallGenerator < Rails::Generators::Base
 
       def add_migrations
-        run 'rake railties:install:migrations FROM=spree_pages'
+        run 'bundle exec rake railties:install:migrations FROM=spree_pages'
       end
 
       def run_migrations
          res = ask "Would you like to run the migrations now? [Y/n]"
          if res == "" || res.downcase == "y"
-           run 'rake db:migrate'
+           run 'bundle exec rake db:migrate'
          else
            puts "Skiping rake db:migrate, don't forget to run it!"
          end
