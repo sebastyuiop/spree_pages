39a082cef118162af3339f2b512b60db3dbe7524
Dan Connor <danconn@danconnor.com>
2013-12-07 01:09:01 -0500

    fix admin override

--- app/overrides/add_pages_tab_to_admin.rb
+++ app/overrides/add_pages_tab_to_admin.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :name => "add_pages_tab_to_admin",
-  :virtual_path => "spree/layouts/admin",
+  :virtual_path => "spree/admin/shared/_menu",
   :insert_bottom => "[data-hook='admin_tabs']",
   :text => %(<%= tab(:pages) %>)
 )
